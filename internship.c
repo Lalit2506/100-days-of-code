f069c8eac96f7f30c48a6bd5b109fbf0c904f415
Lalit2506 <lalitkumar2506@outlook.com>
2025-08-22 07:30:48 +0530

    Update internship.c with new content

--- internship.c
+++ internship.c
@@ -12,5 +12,12 @@ int main()
     printf("sum is (C) : %d\n", c);
     printf("difference is (D) : %d\n", d);
     printf("multiple is (E) : %d\n", e);
+
+    // Example Output:
+    // If a = 5 and b = 3
+    // sum is (C) : 8
+    // difference is (D) : 2
+    // multiple is (E) : 15
+
     return 0;
 }
