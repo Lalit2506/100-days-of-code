2d8208e966d3c8df330162985ea9551870a8a040
Lalit2506 <lalitkumar2506@outlook.com>
2025-08-22 07:32:58 +0530

    Add calculator.c file with basic calculator functionality

---
+++ calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char operator;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator); // space before %c skips newline
+    if (operator == '+') {
+        printf("Result: %d\n", a + b);
+    } else if (operator == '-') {
+        printf("Result: %d\n", a - b);
+    } else if (operator == '*') {
+        printf("Result: %d\n", a * b);
+    } else if (operator == '/') {
+        if (b != 0) {
+            printf("Result: %f\n", (float)a / b);
+        } else {
+            printf("Error: Division by zero.\n");
+        }
+    } else {
+        printf("Invalid operator.\n");
+    }
+    return 0;
+}
